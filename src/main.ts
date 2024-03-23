cc917d4e9916a5c18b7079b6d5156e044ba7c69f
AnuragTiwari1 <anuragkumartiwari79@gmail.com>
2024-03-23 09:15:19 +0530

    added background and preview assets

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,11 @@
 import kaboom from "kaboom";
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
 import { gameScene } from "./scenes/game";
 
 kaboom({
   global: true,
-  width: 320,
-  height: 240,
+  width: 600,
+  height: 600,
   font: "sans-serif",
   canvas: document.querySelector("#myGameCanvas") as any,
-  background: [0, 0, 255],
 });
-
-setGravity(2000);
-loadBean();
-loadSprite("vite", viteLogo);
-
-gameScene();
-
-go("game");
